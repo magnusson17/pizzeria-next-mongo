ca97e237ba2ec28aeb4282840dabcdfcad8600d2
Gabriele Magni <gabri.magni.97@gmail.com>
2025-03-07 09:41:10 +0100

    Modifiche header per tener traccia della locale al click dei link

--- src/app/api/pizzas/route.js
+++ src/app/api/pizzas/route.js
@@ -11,7 +11,7 @@ export async function GET() {
         // .populate: al posto del id di ogni ingrediente associato alla singola pizza
         // popola la key ingredienti con i contenuti ingrediente, in questo caso farà un retrieve solo del id e del "nome"
         const data = await Pizza.find().populate("ingredienti", "nome")
-        
+
         return NextResponse.json({ success: true, data })
     } catch (error) {
         return NextResponse.json({ success: false, error: error.message }, { status: 500 })
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,17 @@
+"use client"
+
+import { useParams } from "next/navigation"
 import Link from "next/link"
 import SwitchLang from "../SwitchLang"
 
 export default function Header() {
+    const { locale } = useParams()
+
     return (
         <header id="header">
             <nav>
-                <Link href="/">Home</Link>
-                <Link href="/pizza">Pizze</Link>
+                <Link href={`/${locale}`}>Home</Link>
+                <Link href={`/${locale}/pizza`}>Pizze</Link>
             </nav>
             <SwitchLang />
         </header>
