e2e1c3ae83f0e6befec3b4c4ba66357bf31d3fab
Gabriele Magni <gabri.magni.97@gmail.com>
2025-03-12 12:02:31 +0100

    risolto(?) problema .populate, ordinato la stampa delle pizze

--- src/app/[locale]/pizza/page.jsx
+++ src/app/[locale]/pizza/page.jsx
@@ -5,23 +5,31 @@ import { GET as handler } from "@/app/api/pizzas/route"
 export default async function Page() {
     const res = await handler()
     const { data } = await res.json()
+    const sortedDataByPrice = data.sort((a, b) => a.prezzo - b.prezzo)
 
     return (
-        <div>
-            <h1>Data from MongoDB</h1>
+        <div className="page-pizzas">
+            <h1>Pizze</h1>
             <ul>
-                {data.map(el => {
+                {sortedDataByPrice.map((pizza, index) => {
                     return (
-                        <li key={el._id}>
-                            <h2>{ el.nome }</h2>
-                            <ul>
-                                {el.ingredienti.map(el => {
-                                    return (
-                                        <li key={el._id}>{el.nome}</li>
-                                    )
-                                })}
-                            </ul>
-                        </li>
+                        <>
+                            {/* stampo il prezzo se l'el prima NON ha un prezzo =, o se l'el è il 1o el */}
+                            { ((sortedDataByPrice[index - 1] && sortedDataByPrice[index].prezzo !== sortedDataByPrice[index - 1].prezzo) || index === 0) ? <h2 className="page-pizzas__price">{pizza.prezzo} &euro;</h2> : '' }
+
+                            <li key={pizza._id}>
+                                <h2>{ pizza.nome }</h2>
+                                <ul className="page-pizzas__ingredients">
+                                    {pizza.ingredienti.map((ing, index) => {
+                                        return (
+                                            <li key={ing._id}>
+                                                {index === pizza.ingredienti.length - 1 ? <span>{ing.nome}</span> : <span>{ing.nome},&nbsp;</span>} 
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        </>
                     )
                 })}
             </ul>
--- src/models/pizzas.js
+++ src/models/pizzas.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import Ingredient from './ingredients'
 
 const pizzaSchema = mongoose.Schema({
     nome: {
