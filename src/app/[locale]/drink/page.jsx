b3d3ad18ece7748884cdf7fd1cefdd2995572853
Gabriele Magni <gabri.magni.97@gmail.com>
2025-03-28 21:10:37 +0100

    pagina drink

---
+++ src/app/[locale]/drink/page.jsx
@@ -0,0 +1,29 @@
+"use server"
+
+import { GET as handler } from "@/app/api/drinks/route"
+
+export async function generateStaticParams() {
+    return [{ locale: "it" }, { locale: "en" }]
+}
+
+export default async function Page({ params }) {
+    const { locale } = await params
+
+    const res = await handler()
+    const { data } = await res.json()
+
+    const drinkSortedByPrice = data.sort((a, b) => a.prezzo - b.prezzo)
+
+    return (
+        <div>
+            <h1>Bevande</h1>
+            <ul>
+                {drinkSortedByPrice.map(drink => {
+                    return (
+                        <li key={drink._id}>{drink.titolo[locale]}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
